148a05b388792f32464efb8e23d559b324f446fe
earlinn <earlinndrow@gmail.com>
2024-05-12 23:24:29 +0500

    Add fixtures for the first pytest test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,94 @@
+import logging
+import os
+from asyncio import AbstractEventLoop
+from collections.abc import AsyncGenerator, Iterator
 
+import pytest
+from aiohttp.pytest_plugin import AiohttpClient
+from aiohttp.test_utils import TestClient, loop_context
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+)
+
+from app.store import Database, Store
+from app.web.app import Application, setup_app
+from app.web.config import Config
+
+
+@pytest.fixture(scope="session")
+def event_loop() -> Iterator[None]:
+    with loop_context() as _loop:
+        yield _loop
+
+
+@pytest.fixture(scope="session")
+def application() -> Application:
+    app = setup_app(
+        config_path=os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "config.yml"
+        )
+    )
+    app.on_startup.clear()
+    app.on_shutdown.clear()
+    app.on_cleanup.clear()
+
+    app.database = Database(app)
+    app.on_startup.append(app.database.connect)  # connect DB
+    app.on_startup.append(app.store.admins.connect)  # create first admin
+
+    app.on_shutdown.append(app.database.disconnect)
+    app.on_shutdown.append(app.store.admins.disconnect)
+    return app
+
+
+@pytest.fixture
+def store(application: Application) -> Store:
+    return application.store
+
+
+@pytest.fixture
+def db_sessionmaker(
+    application: Application,
+) -> async_sessionmaker[AsyncSession]:
+    return application.database.session
+
+
+@pytest.fixture
+def db_engine(application: Application) -> AsyncEngine:
+    return application.database.engine
+
+
+@pytest.fixture(autouse=True)
+async def clear_db(application: Application) -> AsyncGenerator[None]:
+    try:
+        yield
+    except Exception as err:
+        logging.warning(err)
+    finally:
+        session = AsyncSession(application.database.engine)
+        connection = session.connection()
+        for table in application.database._db.metadata.tables:
+            await session.execute(text(f"TRUNCATE {table} CASCADE"))
+            await session.execute(
+                text(f"ALTER SEQUENCE {table}_id_seq RESTART WITH 1")
+            )
+
+        await session.commit()
+        connection.close()
+
+
+@pytest.fixture
+def config(application: Application) -> Config:
+    return application.config
+
+
+@pytest.fixture(autouse=True)
+def cli(
+    aiohttp_client: AiohttpClient,
+    event_loop: AbstractEventLoop,
+    application: Application,
+) -> TestClient:
+    return event_loop.run_until_complete(aiohttp_client(application))
