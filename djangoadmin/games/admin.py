8ac62fcec5bd0049562ae0e224c4f2b9fd314276
earlinn <earlinndrow@gmail.com>
2024-05-23 16:27:33 +0500

    Add Django admin panel microservice

---
+++ djangoadmin/games/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+
+from .models import Balance, Game, GamePlay, Player
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ["pk", "username", "tg_id"]
+    list_display_links = ["username"]
+
+
+@admin.register(Balance)
+class BalanceAdmin(admin.ModelAdmin):
+    list_display = ["pk", "chat_id", "player", "current_value"]
+    list_display_links = ["chat_id", "player"]
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = [
+        "pk",
+        "chat_id",
+        "created_at",
+        "status",
+        "stage",
+        "diller_cards",
+    ]
+    list_display_links = ["chat_id", "created_at"]
+
+
+@admin.register(GamePlay)
+class GamePlayAdmin(admin.ModelAdmin):
+    list_display = [
+        "pk",
+        "game",
+        "player",
+        "player_bet",
+        "player_status",
+        "player_cards",
+    ]
+    list_display_links = ["game", "player"]
